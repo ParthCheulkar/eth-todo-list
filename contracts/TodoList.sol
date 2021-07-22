db369e1187a8f0901622ca2b2fba9460ded1abc9
Parth Cheulkar <pscheulkar12@gmail.com>
2021-07-22 18:59:52 +0530

    first commit

---
+++ contracts/TodoList.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.5.0;
+
+contract TodoList{
+    uint public taskCount = 0;
+
+    struct Task{
+    uint id;
+    string content;
+    bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public{
+        createTask("Check out bruh!!1!!");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
+
+
+}
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,8 @@
+var TodoList = artifacts.require("./TodoList.sol");
+
+module.exports = function(deployer){
+    deployer.deploy(TodoList);
+
+    
+}; 
+
